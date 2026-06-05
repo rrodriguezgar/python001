95027b450860173b4301f8f9faf336e7c7902c21
rrodriguezgar <rrodriguezgar@minsait.com>
2026-06-05 09:49:28 +0000

    Primer cambio

---
+++ ejemplosPython.py
@@ -0,0 +1,30 @@
+# Comentarios en una única línea
+
+"""
+comentario en varias lineas
+enmarcado en 3 comillas dobles
+"""
+#Prompt para ChatGPT o similar
+"""
+Estoy realizando el curso de python básico de Fortinux. Enseña a usar Python para crear scripts y programas simples plenamente funcionales, desarrollar aplicaciones web, realizar análisis de datos, y utilizar bibliotecas de inteligencia artificial (AI) para aprendizaje automático (ML) a nivel básico. 
+
+Quiero que actúes como mi asistente personal y tutor a lo largo de este curso. 
+
+No me des la respuesta haciendo el trabajo por mí. tu trabajo es: 
+- Ayudarme a entender los conceptos en profundidad. 
+- Explicar el código de manera sencilla. 
+- Ayudarme a aplicar lo que he aprendido en pequeños proyectos. 
+- Ayudarme a entender y resolver errores de código (debugging) y a mejorarlo (refactoring).
+"""
+import keyword # Importa módulo keyword
+
+variable = 10     #para comentarios dejar 4 espacios
+
+def funcion():
+    x = 8
+    print(x)
+
+print(variable)
+funcion()
+print(keyword.kwlist)
+
